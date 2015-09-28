52cf6ccde94d99072b785a0f68509f8e0b5fd37c
Christopher Underwood <christopherunderwood@Christophers-MacBook-Pro.local>
2015-09-27 21:42:43 -0700

    renamed files from rental-tile to question-tile

--- tests/integration/components/question-tile-test.js
+++ tests/integration/components/question-tile-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('rental-tile', 'Integration | Component | rental tile', {
+moduleForComponent('question-tile', 'Integration | Component | question tile', {
   integration: true
 });
 
@@ -11,15 +11,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{rental-tile}}`);
+  this.render(hbs`{{question-tile}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#rental-tile}}
+    {{#question-tile}}
       template block text
-    {{/rental-tile}}
+    {{/question-tile}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
